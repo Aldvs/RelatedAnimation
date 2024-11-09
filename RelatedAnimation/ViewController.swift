908d8b2368aaed256a7ccbf4a20dff4bc5257343
Andrew Gogol <andrewgogol777@gmail.com>
2024-11-09 20:25:03 +0300

    Setup slider and square view

--- RelatedAnimation/ViewController.swift
+++ RelatedAnimation/ViewController.swift
@@ -9,18 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let animator = UIViewPropertyAnimator(duration: 0.7, curve: .easeOut)
+    
+    private lazy var squareView: UIView = {
+        let square = UIView()
+        
+        square.backgroundColor = .systemCyan
+        square.layer.cornerRadius = 17
+        
+        
+        return square
+    }()
+    
+    private lazy var sliderView: UISlider = {
+        let slider = UISlider()
+        
+
+        return slider
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemCyan
-        setupRectangle()
+        
+        setupViews()
+        setupAnimator()
     }
     
-    private func setupRectangle() {
-        let rotatingView = UIView()
-        rotatingView.frame = CGRect(x: 50, y: 100, width: 100, height: 100)
-        rotatingView.backgroundColor = UIColor.green
-        rotatingView.layer.cornerRadius = 17
-        view.addSubview(rotatingView)
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        
+        guard squareView.transform == .identity else { return }
+        
+        squareView.frame = .init(x: view.layoutMargins.left, y: 110, width: 80, height: 80)
+        
+        sliderView.sizeToFit()
+        sliderView.frame = .init(
+            x: view.layoutMargins.left,
+            y: squareView.frame.maxY + 44,
+            width: view.frame.width - (view.layoutMargins.left * 2),
+            height: 80
+        )
+        
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .white
+        view.addSubview(squareView)
+        view.addSubview(sliderView)
+    }
+    
+    private func setupAnimator() {
+        
     }
 }
 
