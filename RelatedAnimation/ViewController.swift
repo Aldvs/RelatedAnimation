a0fa2d2263e8ef8c958678a4d706816b77b94c70
Andrew Gogol <andrewgogol777@gmail.com>
2024-11-09 18:27:49 +0300

    Update InitVC with Rect

--- RelatedAnimation/ViewController.swift
+++ RelatedAnimation/ViewController.swift
@@ -11,9 +11,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemCyan
+        setupRectangle()
     }
-
-
+    
+    private func setupRectangle() {
+        let rotatingView = UIView()
+        rotatingView.frame = CGRect(x: 50, y: 100, width: 100, height: 100)
+        rotatingView.backgroundColor = UIColor.green
+        rotatingView.layer.cornerRadius = 17
+        view.addSubview(rotatingView)
+    }
 }
 
